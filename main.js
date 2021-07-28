bbb920fc7e63f5299e4c79711d73c906cb07651b
Caio Zirretta <84157226+CaioZirretta@users.noreply.github.com>
2021-07-28 01:44:09 -0300

    navbar finished
    - Toggle user profile
    - Logout button before the profile
    - Animation
    - Minor CSS improvements and optimization

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ function debounce(func, timeout = 300){
       timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
 }
+
+function minimizeNav(){
+  const liA = document.querySelectorAll('.nav li')
+  for(i of liA){
+    i.classList.toggle('hide')
+  }
+  const navUser = document.querySelector('.nav .user')
+  navUser.classList.toggle('show')
+
+  const navMinimize = document.querySelector('.nav li button i')
+  navMinimize.classList.toggle('rotate')
+
+  const navLogOut = document.querySelector('.nav .logout')
+  navLogOut.classList.toggle('show')
+}
